4c064bf0dfe9a4b6d9a19195fb258d220b117acb
Kyle McLean <kylem590@gmail.com>
2024-12-16 18:41:07 -0700

    Add day 6 part 2

--- day6.ts
+++ day6.ts
@@ -7,21 +7,17 @@ function g(row: number, col: number) {
   return grid[row]?.[col];
 }
 
-const distinctPositions = new Set<`${number},${number}`>();
-
 interface Guard {
   row: number;
   col: number;
   direction: "^" | "v" | "<" | ">";
 }
 
-let guard: Readonly<Guard> = {
-  row: 0,
-  col: 0,
-  direction: "^",
-};
+function serializeGuard(guard: Guard) {
+  return `${guard.row},${guard.col},${guard.direction}` as const;
+}
 
-function moveGuard(guard: Guard): Guard {
+function desiredPosition(guard: Guard) {
   let desiredRow = guard.row;
   let desiredCol = guard.col;
 
@@ -30,7 +26,25 @@ function moveGuard(guard: Guard): Guard {
   else if (guard.direction === ">") desiredCol += 1;
   else if (guard.direction === "<") desiredCol -= 1;
 
-  if (g(desiredRow, desiredCol) === "#") {
+  return { desiredRow, desiredCol };
+}
+
+function moveGuard(
+  guard: Guard,
+  { additionalObstruction } = {
+    additionalObstruction: undefined as
+      | undefined
+      | { row: number; col: number },
+  }
+): Guard {
+  const { desiredRow, desiredCol } = desiredPosition(guard);
+
+  if (
+    g(desiredRow, desiredCol) === "#" ||
+    (additionalObstruction &&
+      additionalObstruction.row === desiredRow &&
+      additionalObstruction.col === desiredCol)
+  ) {
     let newDirection: typeof guard.direction;
     if (guard.direction === "^") newDirection = ">";
     else if (guard.direction === ">") newDirection = "v";
@@ -42,37 +56,89 @@ function moveGuard(guard: Guard): Guard {
   }
 }
 
+function moveGuardUntilDone(
+  guard: Guard,
+  { additionalObstruction } = {
+    additionalObstruction: undefined as
+      | undefined
+      | { row: number; col: number },
+  }
+) {
+  const guardStates = new Set<ReturnType<typeof serializeGuard>>();
+
+  while (true) {
+    guardStates.add(serializeGuard(guard));
+    const newGuard = moveGuard(guard, { additionalObstruction });
+
+    if (
+      newGuard.row < 0 ||
+      newGuard.row >= height ||
+      newGuard.col < 0 ||
+      newGuard.col >= width
+    )
+      return {
+        terminalGuard: guard,
+        guardStates,
+        reason: "leftGrid" as const,
+      };
+
+    if (guardStates.has(serializeGuard(newGuard)))
+      return {
+        terminalGuard: guard,
+        guardStates,
+        reason: "alreadyWasHere" as const,
+      };
+
+    guard = newGuard;
+  }
+}
+
+function findGuard(): Guard {
+  for (let row = 0; row < height; row++) {
+    for (let col = 0; col < width; col++) {
+      if (
+        g(row, col) === "^" ||
+        g(row, col) === "v" ||
+        g(row, col) === ">" ||
+        g(row, col) === "<"
+      ) {
+        return {
+          row,
+          col,
+          direction: g(row, col) as Guard["direction"],
+        };
+      }
+    }
+  }
+  throw new Error("No guard found");
+}
+
+const initialGuard = findGuard();
+
+const result = moveGuardUntilDone(initialGuard);
+if (result.reason === "alreadyWasHere") throw new Error("There is a loop");
+
+const distinctPositions = new Set(
+  result.guardStates
+    .values()
+    .map((guard) => guard.slice(0, guard.lastIndexOf(",")))
+);
+
+const distinctPositionCount = distinctPositions.size;
+console.log("distinctPositionCount", distinctPositionCount);
+
+let addedObstructionCount = 0;
+
 for (let row = 0; row < height; row++) {
   for (let col = 0; col < width; col++) {
+    if (row === initialGuard.row && col === initialGuard.col) continue;
     if (
-      g(row, col) === "^" ||
-      g(row, col) === "v" ||
-      g(row, col) === ">" ||
-      g(row, col) === "<"
+      moveGuardUntilDone(initialGuard, { additionalObstruction: { row, col } })
+        .reason === "alreadyWasHere"
     ) {
-      guard = {
-        row,
-        col,
-        direction: g(row, col) as typeof guard.direction,
-      };
-      break;
+      addedObstructionCount += 1;
     }
   }
 }
 
-while (true) {
-  distinctPositions.add(`${guard.row},${guard.col}`);
-
-  guard = moveGuard(guard);
-
-  if (
-    guard.row < 0 ||
-    guard.row >= height ||
-    guard.col < 0 ||
-    guard.col >= width
-  )
-    break;
-}
-
-const distinctPositionCount = distinctPositions.size;
-console.log("distinctPositionCount", distinctPositionCount);
+console.log("addedObstructionCount", addedObstructionCount);
