a2a382c0531119645124c0e9cb0683e0cc99b6c7
Kyle McLean <kylem590@gmail.com>
2024-12-16 23:29:46 -0700

    Add day 9 part 2

--- day9.ts
+++ day9.ts
@@ -30,6 +30,84 @@ function moveBlocks(disk: number[]) {
   return disk;
 }
 
+function findSlot(
+  disk: number[],
+  startPredicate: (fileId: number) => boolean,
+  start: number,
+  direction: 1 | -1,
+  stop: number,
+  minSize?: number
+) {
+  while (
+    start >= 0 &&
+    start < disk.length &&
+    start !== stop &&
+    !startPredicate(disk[start])
+  )
+    start += direction;
+
+  if (start < 0 || start >= disk.length || start === stop) return null;
+
+  let end = start + direction;
+  while (
+    end >= 0 &&
+    end < disk.length &&
+    start !== stop &&
+    disk[start] === disk[end]
+  )
+    end += direction;
+  end -= direction;
+
+  if (minSize !== undefined && Math.abs(end - start) + 1 < minSize)
+    return findSlot(
+      disk,
+      startPredicate,
+      end + direction,
+      direction,
+      stop,
+      minSize
+    );
+
+  return { left: Math.min(start, end), right: Math.max(start, end) };
+}
+
+function moveFiles(disk: number[]) {
+  disk = [...disk];
+
+  let r = disk.length - 1;
+
+  while (r >= 0) {
+    const lastFile = findSlot(disk, (fileId) => fileId >= 0, r, -1, 0);
+    if (!lastFile) break;
+
+    const firstEmpty = findSlot(
+      disk,
+      (fileId) => fileId < 0,
+      0,
+      1,
+      lastFile.left,
+      lastFile.right - lastFile.left + 1
+    );
+
+    if (firstEmpty) {
+      disk = [
+        ...disk.slice(0, firstEmpty.left),
+        ...disk.slice(lastFile.left, lastFile.right + 1),
+        ...disk.slice(
+          firstEmpty.left + lastFile.right - lastFile.left + 1,
+          lastFile.left
+        ),
+        ...Array(lastFile.right - lastFile.left + 1).fill(-1),
+        ...disk.slice(lastFile.right + 1),
+      ];
+    }
+
+    r = lastFile.left - 1;
+  }
+
+  return disk;
+}
+
 function calculateChecksum(disk: number[]) {
   let checksum = 0;
 
@@ -41,7 +119,8 @@ function calculateChecksum(disk: number[]) {
 }
 
 console.log("moveBlocksChecksum", calculateChecksum(moveBlocks(disk)));
+console.log("moveFilesChecksum", calculateChecksum(moveFiles(disk)));
 
 function printDisk(disk: number[]) {
-  console.log(disk.map((x) => (x >= 0 ? x : ".")).join(""));
+  console.log(disk.map((x) => (x >= 0 ? x.toString()[0] : ".")).join(""));
 }
