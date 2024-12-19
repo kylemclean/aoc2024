027e5d49ff27dcc9dd60a3ddfcaaf2626919e950
Kyle McLean <kylem590@gmail.com>
2024-12-19 00:50:29 -0700

    Refactor day 17 part 1

--- day17.ts
+++ day17.ts
@@ -9,27 +9,16 @@ function parseProgram(line: string) {
   return line.slice(9).split(",").map(Number);
 }
 
-let instructionPointer = 0;
-
 const registerLines = lines.slice(0, lines.indexOf(""));
-const registers = {
+const initialRegisters = {
   A: parseRegister(registerLines[0]),
   B: parseRegister(registerLines[1]),
   C: parseRegister(registerLines[2]),
 };
+type Registers = typeof initialRegisters;
 
 const programLine = lines[lines.indexOf("") + 1];
 const program = parseProgram(programLine);
-
-const output: number[] = [];
-
-function comboOperandValue(operand: number) {
-  if (operand === 4) return registers.A;
-  else if (operand === 5) return registers.B;
-  else if (operand === 6) return registers.C;
-
-  return operand;
-}
 
 const Opcode: Readonly<Record<string, number>> = {
   ADV: 0,
@@ -42,39 +31,78 @@ const Opcode: Readonly<Record<string, number>> = {
   CDV: 7,
 };
 
-function divideInstruction(operandData: number) {
-  const operandValue = comboOperandValue(operandData);
+function comboOperandValue(registers: Registers, operand: number) {
+  if (operand === 4) return registers.A;
+  else if (operand === 5) return registers.B;
+  else if (operand === 6) return registers.C;
+
+  return operand;
+}
+
+function divideInstruction(registers: Registers, operandData: number) {
+  const operandValue = comboOperandValue(registers, operandData);
   const denominator = 1 << operandValue;
   return Math.floor(registers.A / denominator);
 }
 
-while (instructionPointer < program.length) {
-  const opcode = program[instructionPointer];
-  const operandData = program[instructionPointer + 1];
+function executeProgram(
+  program: number[],
+  registers: { A: number; B: number; C: number }
+) {
+  registers = { ...registers };
+  const output: number[] = [];
+  let instructionPointer = 0;
 
-  if (opcode === Opcode.ADV) {
-    registers.A = divideInstruction(operandData);
-  } else if (opcode === Opcode.BXL) {
-    const operandValue = operandData;
-    registers.B = registers.B ^ operandValue;
-  } else if (opcode === Opcode.BST) {
-    const operand = comboOperandValue(operandData);
-    registers.B = operand % 8;
-  } else if (opcode === Opcode.JNZ) {
-    const operand = operandData;
-    if (registers.A !== 0) instructionPointer = operand - 2;
-  } else if (opcode === Opcode.BXC) {
-    registers.B = registers.B ^ registers.C;
-  } else if (opcode === Opcode.OUT) {
-    const operand = comboOperandValue(operandData);
-    output.push(operand % 8);
-  } else if (opcode === Opcode.BDV) {
-    registers.B = divideInstruction(operandData);
-  } else if (opcode === Opcode.CDV) {
-    registers.C = divideInstruction(operandData);
+  while (instructionPointer < program.length) {
+    const opcode = program[instructionPointer];
+    const operandData = program[instructionPointer + 1];
+
+    if (opcode === Opcode.ADV) {
+      registers.A = divideInstruction(registers, operandData);
+    } else if (opcode === Opcode.BXL) {
+      const operandValue = operandData;
+      registers.B = registers.B ^ operandValue;
+    } else if (opcode === Opcode.BST) {
+      const operand = comboOperandValue(registers, operandData);
+      registers.B = operand % 8;
+    } else if (opcode === Opcode.JNZ) {
+      const operand = operandData;
+      if (registers.A !== 0) instructionPointer = operand - 2;
+    } else if (opcode === Opcode.BXC) {
+      registers.B = registers.B ^ registers.C;
+    } else if (opcode === Opcode.OUT) {
+      const operand = comboOperandValue(registers, operandData);
+      output.push(operand % 8);
+    } else if (opcode === Opcode.BDV) {
+      registers.B = divideInstruction(registers, operandData);
+    } else if (opcode === Opcode.CDV) {
+      registers.C = divideInstruction(registers, operandData);
+    }
+
+    instructionPointer += 2;
   }
 
-  instructionPointer += 2;
+  return output;
 }
 
-console.log(output.join(","));
+console.log("part1", executeProgram(program, initialRegisters).join(","));
+
+for (let a = 1; ; a++) {
+  // console.log(a);
+  const registers = { ...initialRegisters, A: a };
+  const output = executeProgram(program, registers);
+
+  let outputMatchesProgram = true;
+  for (let i = 0; i < Math.max(output.length, program.length); i++) {
+    11;
+    if (output[i] !== program[i]) {
+      outputMatchesProgram = false;
+      break;
+    }
+  }
+
+  if (outputMatchesProgram) {
+    console.log("part2", a);
+    break;
+  }
+}
