c78d01db319371bb490809dfb30484a800062484
Kyle McLean <kylem590@gmail.com>
2024-12-19 03:17:49 -0700

    Add day 17 part 2

--- day17.ts
+++ day17.ts
@@ -2,11 +2,11 @@ const text = await Bun.file("day17input.txt").text();
 const lines = text.split("\n");
 
 function parseRegister(line: string) {
-  return Number(line.match(/Register .: (?<value>[0-9]+)/)?.groups?.value ?? 0);
+  return BigInt(line.match(/Register .: (?<value>[0-9]+)/)?.groups?.value ?? 0);
 }
 
 function parseProgram(line: string) {
-  return line.slice(9).split(",").map(Number);
+  return line.slice(9).split(",").map(BigInt);
 }
 
 const registerLines = lines.slice(0, lines.indexOf(""));
@@ -20,37 +20,34 @@ type Registers = typeof initialRegisters;
 const programLine = lines[lines.indexOf("") + 1];
 const program = parseProgram(programLine);
 
-const Opcode: Readonly<Record<string, number>> = {
-  ADV: 0,
-  BXL: 1,
-  BST: 2,
-  JNZ: 3,
-  BXC: 4,
-  OUT: 5,
-  BDV: 6,
-  CDV: 7,
+const Opcode: Readonly<Record<string, bigint>> = {
+  ADV: 0n,
+  BXL: 1n,
+  BST: 2n,
+  JNZ: 3n,
+  BXC: 4n,
+  OUT: 5n,
+  BDV: 6n,
+  CDV: 7n,
 };
 
-function comboOperandValue(registers: Registers, operand: number) {
-  if (operand === 4) return registers.A;
-  else if (operand === 5) return registers.B;
-  else if (operand === 6) return registers.C;
+function comboOperandValue(registers: Registers, operand: bigint) {
+  if (operand === 4n) return registers.A;
+  else if (operand === 5n) return registers.B;
+  else if (operand === 6n) return registers.C;
 
   return operand;
 }
 
-function divideInstruction(registers: Registers, operandData: number) {
+function divideInstruction(registers: Registers, operandData: bigint) {
   const operandValue = comboOperandValue(registers, operandData);
-  const denominator = 1 << operandValue;
-  return Math.floor(registers.A / denominator);
+  const denominator = 2n ** operandValue;
+  return registers.A / denominator;
 }
 
-function executeProgram(
-  program: number[],
-  registers: { A: number; B: number; C: number }
-) {
+function executeProgram(program: bigint[], registers: Registers) {
   registers = { ...registers };
-  const output: number[] = [];
+  const output: bigint[] = [];
   let instructionPointer = 0;
 
   while (instructionPointer < program.length) {
@@ -64,15 +61,15 @@ function executeProgram(
       registers.B = registers.B ^ operandValue;
     } else if (opcode === Opcode.BST) {
       const operand = comboOperandValue(registers, operandData);
-      registers.B = operand % 8;
+      registers.B = operand % 8n;
     } else if (opcode === Opcode.JNZ) {
       const operand = operandData;
-      if (registers.A !== 0) instructionPointer = operand - 2;
+      if (registers.A !== 0n) instructionPointer = Number(operand - 2n);
     } else if (opcode === Opcode.BXC) {
       registers.B = registers.B ^ registers.C;
     } else if (opcode === Opcode.OUT) {
       const operand = comboOperandValue(registers, operandData);
-      output.push(operand % 8);
+      output.push(operand % 8n);
     } else if (opcode === Opcode.BDV) {
       registers.B = divideInstruction(registers, operandData);
     } else if (opcode === Opcode.CDV) {
@@ -87,22 +84,51 @@ function executeProgram(
 
 console.log("part1", executeProgram(program, initialRegisters).join(","));
 
-for (let a = 1; ; a++) {
-  // console.log(a);
-  const registers = { ...initialRegisters, A: a };
-  const output = executeProgram(program, registers);
+function aThatProduces(desiredOutput: bigint[], existingA: bigint) {
+  const candidates: bigint[] = [];
 
-  let outputMatchesProgram = true;
-  for (let i = 0; i < Math.max(output.length, program.length); i++) {
-    11;
-    if (output[i] !== program[i]) {
-      outputMatchesProgram = false;
-      break;
+  for (let i = 0n; i < 2n ** 3n; i++) {
+    const programOutput = executeProgram(program, {
+      ...initialRegisters,
+      A: existingA + i,
+    });
+
+    let matches = true;
+
+    for (
+      let j = 0;
+      j < Math.max(desiredOutput.length, programOutput.length);
+      j++
+    ) {
+      if (desiredOutput[j] !== programOutput[j]) {
+        matches = false;
+        break;
+      }
     }
+
+    if (matches) candidates.push(i);
   }
 
-  if (outputMatchesProgram) {
-    console.log("part2", a);
-    break;
-  }
+  return candidates;
 }
+
+function findA(bytesOfProgram = 1, existingA = 0n): bigint[] {
+  if (bytesOfProgram > program.length) return [existingA / 2n ** 3n];
+
+  const slice = program.slice(program.length - bytesOfProgram);
+  const aThatProducesNumber = aThatProduces(slice, existingA);
+  const results = aThatProducesNumber.flatMap((a) =>
+    findA(bytesOfProgram + 1, existingA * 2n ** 3n + a * 2n ** 3n)
+  );
+  return results;
+}
+
+const as = findA();
+if (as.length === 0) throw new Error("no A");
+const A = as.toSorted()[0];
+
+console.log(
+  "part2",
+  A.toString(),
+  executeProgram(program, { ...initialRegisters, A })
+);
