62fe5dfc50e2f7758c452ee8285ca36f9c3b4e71
Kyle McLean <kylem590@gmail.com>
2024-12-21 23:15:55 -0700

    Add day 22 part 2

--- day22.ts
+++ day22.ts
@@ -1,23 +1,68 @@
 const text = await Bun.file("day22input.txt").text();
-const initialSecrets = text.split("\n").filter(Boolean).map(BigInt);
+const initialSecrets = text.split("\n").filter(Boolean).map(Number);
 
-function nextSecret(secret: bigint): bigint {
-  secret ^= secret * 64n;
-  secret %= 16777216n;
-  secret ^= secret / 32n;
-  secret %= 16777216n;
-  secret ^= secret * 2048n;
-  secret %= 16777216n;
-  return secret;
+function nextSecret(secret: number): number {
+  let secretN = BigInt(secret);
+  secretN ^= secretN * 64n;
+  secretN %= 16777216n;
+  secretN ^= secretN / 32n;
+  secretN %= 16777216n;
+  secretN ^= secretN * 2048n;
+  secretN %= 16777216n;
+  return Number(secretN);
 }
 
-function nthSecret(initialSecret: bigint, n: number): bigint {
-  if (n === 0) return initialSecret;
-  return nthSecret(nextSecret(initialSecret), n - 1);
+function getSequence(secret: number, n: number) {
+  const sequence: number[] = [secret];
+  while (sequence.length <= n) {
+    secret = nextSecret(secret);
+    sequence.push(secret);
+  }
+  return sequence;
 }
 
-const sum = initialSecrets.reduce(
-  (acc, secret) => acc + nthSecret(secret, 2000),
-  0n
+const secretSequences = initialSecrets.map((secret) =>
+  getSequence(secret, 2000)
+);
+
+const sum = secretSequences.reduce(
+  (acc, secretSequence) => acc + secretSequence.at(-1)!,
+  0
 );
 console.log("part1", sum.toString());
+
+function price(secret: number) {
+  return secret % 10;
+}
+
+const changeSequencePrices = new Map<string, number>();
+
+for (const secretSequence of secretSequences) {
+  const seenChangeSequencesForThisSecretSequence = new Set<string>();
+
+  const changeSequence: number[] = [];
+  for (let i = 1; i < secretSequence.length; i++) {
+    changeSequence.push(
+      price(secretSequence[i]) - price(secretSequence[i - 1])
+    );
+    if (changeSequence.length > 4) changeSequence.shift();
+    if (changeSequence.length < 4) continue;
+
+    const changeSequenceString = changeSequence.join(",");
+    if (seenChangeSequencesForThisSecretSequence.has(changeSequenceString))
+      continue;
+    seenChangeSequencesForThisSecretSequence.add(changeSequenceString);
+
+    changeSequencePrices.set(
+      changeSequenceString,
+      (changeSequencePrices.get(changeSequenceString) ?? 0) +
+        price(secretSequence[i])
+    );
+  }
+}
+
+const sortedChangeSequencePrices = changeSequencePrices
+  .entries()
+  .toArray()
+  .toSorted((a, b) => b[1] - a[1]);
+console.log("sortedChangeSequencePrices", sortedChangeSequencePrices[0]);
