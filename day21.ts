e145dee0d2e10c9f77183b1d3c8b4b379b322e37
Kyle McLean <kylem590@gmail.com>
2024-12-21 20:47:51 -0700

    Add day 21 part 2

--- day21.ts
+++ day21.ts
@@ -1,212 +1,262 @@
 const text = await Bun.file("day21input.txt").text();
-const codes = text
-  .split("\n")
-  .filter(Boolean)
-  .map((line) => line.split("")) as ReadonlyArray<ReadonlyArray<CodeButton>>;
+const codes = text.split("\n").filter(Boolean);
 
-type Keypad = ReadonlyArray<ReadonlyArray<string>>;
+function memoize<T extends (...args: any[]) => any>(f: T): T {
+  const cache = new Map<string, ReturnType<T>>();
 
-const codeKeypad: Keypad = [
-  ["7", "8", "9"],
-  ["4", "5", "6"],
-  ["1", "2", "3"],
-  ["", "0", "A"],
-];
+  return function (...args: Parameters<T>): ReturnType<T> {
+    const key = JSON.stringify(args);
+    if (cache.has(key)) return cache.get(key)!;
 
-type CodeButton =
-  | "7"
-  | "8"
-  | "9"
-  | "4"
-  | "5"
-  | "6"
-  | "1"
-  | "2"
-  | "3"
-  | "0"
-  | "A";
+    const result = f(...args);
+    cache.set(key, result);
+    return result;
+  } as T;
+}
 
-const directionalKeypad: Keypad = [
-  ["", "^", "A"],
-  ["<", "v", ">"],
-];
+const codeTransitions: Record<string, Record<string, string[]>> = {
+  "0": {
+    "0": [""],
+    "1": ["^<"],
+    "2": ["^"],
+    "3": ["^>", ">^"],
+    "4": ["^^<"],
+    "5": ["^^"],
+    "6": ["^^>", ">^^"],
+    "7": ["^^^<"],
+    "8": ["^^^"],
+    "9": ["^^^>", ">^^^"],
+    A: [">"],
+  },
+  "1": {
+    "0": [">v"],
+    "1": [""],
+    "2": [">"],
+    "3": [">>"],
+    "4": ["^"],
+    "5": ["^>", ">^"],
+    "6": ["^>>", ">>^"],
+    "7": ["^^"],
+    "8": ["^^>", ">^^"],
+    "9": ["^^>>", ">>^^"],
+    A: [">>v"],
+  },
+  "2": {
+    "0": ["v"],
+    "1": ["<"],
+    "2": [""],
+    "3": [">"],
+    "4": ["^<", "<^"],
+    "5": ["^"],
+    "6": ["^>", ">^"],
+    "7": ["^^<", "<^^"],
+    "8": ["^^"],
+    "9": ["^^>", ">^^"],
+    A: [">v", "v>"],
+  },
+  "3": {
+    "0": ["<v", "v<"],
+    "1": ["<<"],
+    "2": ["<"],
+    "3": [""],
+    "4": ["^<<", "<<^"],
+    "5": ["^<", "<^"],
+    "6": ["^"],
+    "7": ["^^<<", "<<^^"],
+    "8": ["^^<", "<^^"],
+    "9": ["^^"],
+    A: ["v"],
+  },
+  "4": {
+    "0": [">vv"],
+    "1": ["v"],
+    "2": [">v", "v>"],
+    "3": [">>v", "v>>"],
+    "4": [""],
+    "5": [">"],
+    "6": [">>"],
+    "7": ["^"],
+    "8": ["^>", ">^"],
+    "9": ["^>>", ">>^"],
+    A: [">>vv"],
+  },
+  "5": {
+    "0": ["vv"],
+    "1": ["<v", "v<"],
+    "2": ["v"],
+    "3": [">v", "v>"],
+    "4": ["<"],
+    "5": [""],
+    "6": [">"],
+    "7": ["^<", "<^"],
+    "8": ["^"],
+    "9": ["^>", ">^"],
+    A: [">vv", "vv>"],
+  },
+  "6": {
+    "0": ["<vv", "vv<"],
+    "1": ["<<v", "v<<"],
+    "2": ["<v", "v<"],
+    "3": ["v"],
+    "4": ["<<"],
+    "5": ["<"],
+    "6": [""],
+    "7": ["^<<", "<<^"],
+    "8": ["^<", "<^"],
+    "9": ["^"],
+    A: ["vv"],
+  },
+  "7": {
+    "0": [">vvv"],
+    "1": ["vv"],
+    "2": [">vv", "vv>"],
+    "3": [">>vv", "vv>>"],
+    "4": ["v"],
+    "5": [">v", "v>"],
+    "6": [">>v", "v>>"],
+    "7": [""],
+    "8": [">"],
+    "9": [">>"],
+    A: [">>vvv"],
+  },
+  "8": {
+    "0": ["vvv"],
+    "1": ["<vv", "vv<"],
+    "2": ["vv"],
+    "3": [">vv", "vv>"],
+    "4": ["<v", "v<"],
+    "5": ["v"],
+    "6": [">v", "v>"],
+    "7": ["<"],
+    "8": [""],
+    "9": [">"],
+    A: [">vvv", "vvv>"],
+  },
+  "9": {
+    "0": ["<vvv", "vvv<"],
+    "1": ["<<vv", "vv<<"],
+    "2": ["<vv", "vv<"],
+    "3": ["vv"],
+    "4": ["<<v", "v<<"],
+    "5": ["<v", "v<"],
+    "6": ["v"],
+    "7": ["<<"],
+    "8": ["<"],
+    "9": [""],
+    A: ["vvv"],
+  },
+  A: {
+    "0": ["<"],
+    "1": ["^<<"],
+    "2": ["^<", "<^"],
+    "3": ["^"],
+    "4": ["^^<<"],
+    "5": ["^^<", "<^^"],
+    "6": ["^^"],
+    "7": ["^^^<<"],
+    "8": ["^^^<", "<^^^"],
+    "9": ["^^^"],
+    A: [""],
+  },
+};
 
-type DirectionalButton = "^" | "<" | ">" | "v" | "A";
+const directionalTransitions: Record<string, Record<string, string[]>> = {
+  "^": {
+    "^": [""],
+    v: ["v"],
+    "<": ["v<"],
+    ">": ["v>", ">v"],
+    A: [">"],
+  },
+  v: {
+    "^": ["^"],
+    v: [""],
+    "<": ["<"],
+    ">": [">"],
+    A: [">^", "^>"],
+  },
+  "<": {
+    "^": [">^"],
+    v: [">"],
+    "<": [""],
+    ">": [">>"],
+    A: [">>^"],
+  },
+  ">": {
+    "^": ["<^", "^<"],
+    v: ["<"],
+    "<": ["<<"],
+    ">": [""],
+    A: ["^"],
+  },
+  A: {
+    "^": ["<"],
+    v: ["v<", "<v"],
+    "<": ["v<<"],
+    ">": ["v"],
+    A: [""],
+  },
+};
 
-type KeypadState = Readonly<{ keypad: Keypad; row: number; col: number }>;
-type State = Readonly<[KeypadState, KeypadState, KeypadState]>;
-type StateString =
-  `${number},${number}|${number},${number}|${number},${number}`;
+const countPresses = memoize((current: string, goal: string, depth: number) => {
+  const transitions = directionalTransitions[current][goal];
 
-function find(keypad: Keypad, button: string) {
-  for (let row = 0; row < keypad.length; row++) {
-    for (let col = 0; col < keypad[row].length; col++) {
-      if (keypad[row][col] === button) return { row, col };
+  function f(transition: string) {
+    if (depth === 0) {
+      return transition.length;
     }
-  }
 
-  throw new Error(`No ${button} found`);
-}
-
-function stateString(state: State) {
-  return state
-    .map((keypadState) => `${keypadState.row},${keypadState.col}`)
-    .join("|") as StateString;
-}
-
-function stateFromString(stateString: StateString) {
-  const [row1, col1, row2, col2, row3, col3] = stateString
-    .split("|")
-    .map(Number);
-  return [
-    { keypad: codeKeypad, row: row1, col: col1 },
-    { keypad: directionalKeypad, row: row2, col: col2 },
-    { keypad: directionalKeypad, row: row3, col: col3 },
-  ] as State;
-}
-
-function validKeypadState({ keypad, row, col }: KeypadState) {
-  return (
-    row >= 0 &&
-    row < keypad.length &&
-    col >= 0 &&
-    col < keypad[row].length &&
-    keypad[row][col]
-  );
-}
-
-function nextState(
-  keypadStates: ReadonlyArray<KeypadState>,
-  button: DirectionalButton,
-  didCodeButtonPress?: (codeButton: CodeButton) => void
-): ReadonlyArray<KeypadState> | undefined {
-  const lastState = keypadStates.at(-1);
-  if (!lastState) return keypadStates;
-
-  const change = {
-    "^": [-1, 0],
-    "<": [0, -1],
-    ">": [0, 1],
-    v: [1, 0],
-    A: [0, 0],
-  }[button];
-
-  const newLastState = {
-    ...lastState,
-    row: lastState.row + change[0],
-    col: lastState.col + change[1],
-  };
-
-  if (!validKeypadState(newLastState)) return undefined;
-
-  if (button === "A") {
-    if (newLastState.keypad === codeKeypad) {
-      didCodeButtonPress?.(
-        newLastState.keypad[newLastState.row][newLastState.col] as CodeButton
+    let total = 0;
+    for (let i = 0; i < transition.length; i++) {
+      total += countPresses(
+        i > 0 ? transition[i - 1] : "A",
+        transition[i],
+        depth - 1
       );
     }
-
-    const buttonToPressOnPreviousKeypad = newLastState.keypad[newLastState.row][
-      newLastState.col
-    ] as DirectionalButton;
-    const nextPreviousStates = nextState(
-      keypadStates.slice(0, -1),
-      buttonToPressOnPreviousKeypad,
-      didCodeButtonPress
-    );
-    if (!nextPreviousStates) return undefined;
-    return [...nextPreviousStates, newLastState];
+    return total;
   }
 
-  return [...keypadStates.slice(0, -1), newLastState];
-}
+  return transitions.reduce((acc, transition) => {
+    const ff = f(transition + "A");
+    if (ff < acc) return ff;
+    return acc;
+  }, Infinity);
+});
 
-function dijkstra(start: State, goal: State) {
-  const costTo = new Map<StateString, number>();
-  const visited = new Set<StateString>();
-  const prev = new Map<
-    StateString,
-    { state: State; byPressingButton: DirectionalButton }
-  >();
+function calculateTotalComplexity(codes: string[], robotCount: number) {
+  let totalComplexity = 0;
 
-  const queue: State[] = [start];
-  costTo.set(stateString(start), 0);
+  for (const code of codes) {
+    let presses = 0;
+    for (let i = 0; i < code.length; i++) {
+      const transitions = codeTransitions[i > 0 ? code[i - 1] : "A"][code[i]];
 
-  while (queue.length > 0) {
-    const current = queue.shift()!;
-
-    const neighborStates = (["^", "<", ">", "v", "A"] as const)
-      .map((button) => ({
-        button,
-        state: nextState(current, button) as State,
-      }))
-      .filter(
-        ({ state }) => state !== undefined && !visited.has(stateString(state))
-      );
-
-    for (const neighbor of neighborStates) {
-      visited.add(stateString(neighbor.state));
-      queue.push(neighbor.state);
-
-      const alt = (costTo.get(stateString(current)) ?? Infinity) + 1;
-      if (alt < (costTo.get(stateString(neighbor.state)) ?? Infinity)) {
-        costTo.set(stateString(neighbor.state), alt);
-
-        prev.set(stateString(neighbor.state), {
-          state: current,
-          byPressingButton: neighbor.button,
-        } as const);
+      function f(transition: string) {
+        let c = 0;
+        for (let j = 0; j < transition.length; j++) {
+          c += countPresses(
+            j > 0 ? transition[j - 1] : "A",
+            transition[j],
+            robotCount - 1
+          );
+        }
+        return c;
       }
+
+      presses += transitions.reduce((acc, transition) => {
+        const ff = f(transition + "A");
+        if (ff < acc) return ff;
+        return acc;
+      }, Infinity);
     }
+
+    const complexity = presses * Number(code.split("A")[0]);
+    totalComplexity += complexity;
   }
 
-  const minCost = costTo.get(stateString(goal)) ?? Infinity;
-
-  const path: DirectionalButton[] = [];
-
-  let current = goal;
-  while (current !== start) {
-    const x = prev.get(stateString(current));
-    if (!x) break;
-    path.push(x.byPressingButton);
-    current = x.state;
-  }
-  path.reverse();
-
-  return { minCost, path };
+  return totalComplexity;
 }
 
-function doPresses(state: State, buttons: DirectionalButton[] | string) {
-  for (const c of buttons)
-    state = nextState(state, c as DirectionalButton) as State;
-  return state;
-}
-
-function complexity(code: ReadonlyArray<CodeButton>) {
-  let state: State = [
-    { keypad: codeKeypad, row: 3, col: 2 },
-    { keypad: directionalKeypad, row: 0, col: 2 },
-    { keypad: directionalKeypad, row: 0, col: 2 },
-  ];
-
-  const sequence: DirectionalButton[] = [];
-  for (const c of code) {
-    const { path } = dijkstra(state, [
-      { keypad: codeKeypad, ...find(codeKeypad, c) },
-      { keypad: directionalKeypad, row: 0, col: 2 },
-      { keypad: directionalKeypad, row: 0, col: 2 },
-    ]);
-    state = doPresses(state, path);
-    sequence.push(...path, "A");
-    state = doPresses(state, "A");
-  }
-
-  console.log(code, sequence.length, sequence.join(""));
-
-  return (
-    sequence.length * parseInt(code.slice(0, code.length - 1).join(""), 10)
-  );
-}
-
-const complexitySum = codes.reduce((acc, code) => acc + complexity(code), 0);
-console.log("complexitySum", complexitySum);
+console.log("totalComplexity2", calculateTotalComplexity(codes, 2));
+console.log("totalComplexity25", calculateTotalComplexity(codes, 25));
